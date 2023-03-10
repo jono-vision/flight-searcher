8559431f5362eb876f7b38db8c6f580be7ca2d63
Jonathan D <jonathandown7@gmail.com>
2023-03-09 21:45:42 -0400

    add message about running csv_gui file if no data file found

--- flight_searcher/csv_reader.py
+++ flight_searcher/csv_reader.py
@@ -14,6 +14,6 @@ def read_csv():
 def _get_flight_data_filepath():
     data_filepath = Path(__file__).resolve().parent / 'flight_data.csv'
     if not data_filepath.is_file():
-        print('No flight data found creating new file')
+        print('No flight data found creating new file. Please add items to the file by running the csv_gui.py file')
         data_filepath.touch()
     return data_filepath
